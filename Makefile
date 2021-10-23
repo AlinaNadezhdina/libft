196aa5bf35415c61422cac479ef7fc3d8d7d93bf
Wendell Collen <wcollen@va-b3.kzn.21-school.ru>
2021-10-23 20:27:24 +0300

    ft_lstadd_back.c

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: wcollen <marvin@42.fr>                     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/10/23 16:07:46 by wcollen           #+#    #+#              #
-#    Updated: 2021/10/23 17:00:29 by wcollen          ###   ########.fr        #
+#    Updated: 2021/10/23 19:20:13 by wcollen          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -27,7 +27,8 @@ SRCS		=	ft_isalpha.c	ft_isdigit.c	ft_isalnum.c\
 
 OBJS		=	$(SRCS:.c=.o)
 
-BONUS		=	ft_lstnew.c	ft_lstadd_front.c\
+BONUS		=	ft_lstnew.c		ft_lstadd_front.c	ft_lstsize.c\
+				ft_lstlast.c	ft_lstadd_back.c
 
 BONUS_OBJS	=	$(BONUS:.c=.o)
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: wcollen <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 17:13:27 by wcollen           #+#    #+#             */
-/*   Updated: 2021/10/23 17:01:15 by wcollen          ###   ########.fr       */
+/*   Updated: 2021/10/23 19:18:32 by wcollen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_H
@@ -54,4 +54,11 @@ void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
 void ft_lstadd_front(t_list **lst, t_list *new);
+int ft_lstsize(t_list *lst);
+t_list *ft_lstlast(t_list *lst);
+void ft_lstadd_back(t_list **lst, t_list *new);
+/*void ft_lstdelone(t_list *lst, void (*del)(void*));
+void ft_lstclear(t_list **lst, void (*del)(void*));
+void ft_lstiter(t_list *lst, void (*f)(void *));
+t_list *ft_lstmap(t_list *lst, void *(*f)(void *),void (*del)(void *));*/
 #endif
